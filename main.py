640a1d3407bac437a6d909e44ffc691d62da43f6
Enzo <75796536+enzo405@users.noreply.github.com>
2022-12-31 00:20:21 +0100

    3.4 finis

--- main.py
+++ main.py
@@ -1,178 +1,104 @@
 import random
 
 class Personnage():
-    def __init__(self, pseudo:str, niveau:int, pdv:int, initiative:int, mana = 0) -> None:
+    def __init__(self, pseudo:str, level:int, hp:int, initiative:int, mana = 0) -> None:
         self.pseudo = pseudo
-        self.niveau = niveau
-        self.pdv = pdv
+        self.level = level
+        self.hp = hp
         self.initiative = initiative
         self.mana = mana
 
     def __str__(self) -> str:
-        return (f"{self.pseudo} niveau {self.niveau} avec {self.pdv} points de vie. Initiative {self.initiative}")
+        return (f"Type Normal => {self.pseudo} level {self.level} with {self.hp} health. Initiative {self.initiative}. Mana {self.mana}")
 
     def checkIfDead(player):
-        if player.pdv <= 0:
+        if player.hp <= 0:
             print(f'{player.pseudo} is dead !!')
             return False
         else:
             return True
 
-    def attaquer(player1, player2):
+    def damage(self):
+        damage = self.level
+        return damage
+
+    def attack(player1, player2):
         if int(player1.initiative) > int(player2.initiative) and player1.checkIfDead() and player2.checkIfDead():
-            player2.pdv = int(player2.pdv) - int(player1.niveau)
-            print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-            if player2.pdv <= 0:
+            player2.hp = int(player2.hp) - int(player1.damage())
+            print(f"-{player1.damage()} hp to {player2.pseudo} (Current Hp : {player2.hp})")
+            if player2.hp <= 0:
+                player2.checkIfDead()
                 return
             else:
-                player1.pdv = int(player1.pdv) - int(player2.niveau)
-                print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
+                player1.hp = int(player1.hp) - int(player2.damage())
+                print(f"-{player2.damage()} hp to {player1.pseudo} (Current Hp : {player1.hp})")
                 player1.checkIfDead()
         elif int(player1.initiative) < int(player2.initiative) and player2.checkIfDead() and player1.checkIfDead():
-            player1.pdv = int(player1.pdv) - int(player2.niveau)
-            print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-            if player1.pdv <= 0:
+            player1.hp = int(player1.hp) - int(player2.damage())
+            print(f"-{player2.damage()} hp to {player1.pseudo} (Current Hp : {player1.hp})")
+            if player1.hp <= 0:
+                player2.checkIfDead()
                 return
             else:
-                player2.pdv = int(player2.pdv) - int(player1.niveau)
-                print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
+                player2.hp = int(player2.hp) - int(player1.damage())
+                print(f"-{player1.damage()} hp to {player2.pseudo} (Current Hp : {player2.hp})")
                 player2.checkIfDead()
         elif int(player1.initiative) == int(player2.initiative):
-            player1.pdv = int(player1.pdv) - int(player2.niveau)
-            print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-            player2.pdv = int(player2.pdv) - int(player1.niveau)
-            print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
+            player1.hp = int(player1.hp) - int(player2.damage())
+            print(f"-{player2.damage()} hp to {player1.pseudo} (Current Hp : {player1.hp})")
+            player2.hp = int(player2.hp) - int(player1.level)
+            print(f"-{player1.damage()} hp to {player2.pseudo} (Current Hp : {player2.hp})")
         return
 
-    def combat(player1, player2):
-        while int(player1.pdv) > 0 and int(player2.pdv) > 0:
-            player1.attaquer(player2)
+    def fight(player1, player2):
+        while int(player1.hp) > 0 and int(player2.hp) > 0:
+            player1.attack(player2)
 
-    def soigner(self):
-        soin = random.randint(1,self.niveau)
-        if self.pdv > 0:
-            self.pdv = self.pdv + soin
+    def heal(self):
+        soin = random.randint(1,self.level)
+        if self.hp > 0:
+            self.hp = self.hp + soin
             print(f"{self.pseudo} just healed himself. HP +{soin}")
-        elif self.pdv <= 0:
+        elif self.hp <= 0:
             print(f"{self.pseudo} is dead and can't heal")
         return
-
 
 
 class Guerrier(Personnage):
-    def __init__(self, pseudo:str, niveau:int, pdv:int, initiative:int) -> None:
-        super().__init__(pseudo, niveau, pdv, initiative)
+    def __init__(self, pseudo:str, level:int, hp:int, initiative:int) -> None:
+        super().__init__(pseudo, level, hp, initiative)
         self.pseudo = str(pseudo)
-        self.niveau = int(niveau)
-        self.pdv = int(niveau) * 8 + 4
-        self.initiative = int(niveau) * 4 + 6
+        self.level = int(level)
+        self.hp = int(level) * 8 + 4
+        self.initiative = int(level) * 4 + 6
 
     def __str__(self) -> str:
-        return (f"Type Guerrier => {self.pseudo} niveau {self.niveau} avec {self.pdv} points de vie. Initiative {self.initiative}")
+        return (f"Type Guerrier => {self.pseudo} level {self.level} with {self.hp} health. Initiative {self.initiative}. Mana {self.mana}")
 
-    def checkIfDead(player):
-        if player.pdv <= 0:
-            print(f'{player.pseudo} is dead !!')
-            return False
-        else:
-            return True
+    def damage(self):
+        damage = self.level * 2
+        return damage
 
-    def attaquer(player1, player2):
-        if int(player1.initiative) > int(player2.initiative) and player1.checkIfDead() and player2.checkIfDead():
-            player2.pdv = int(player2.pdv) - int(player1.niveau)
-            print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-            if player2.pdv <= 0:
-                return
-            else:
-                player1.pdv = int(player1.pdv) - int(player2.niveau)
-                print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-                player1.checkIfDead()
-        elif int(player1.initiative) < int(player2.initiative) and player2.checkIfDead() and player1.checkIfDead():
-            player1.pdv = int(player1.pdv) - int(player2.niveau)
-            print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-            if player1.pdv <= 0:
-                return
-            else:
-                player2.pdv = int(player2.pdv) - int(player1.niveau)
-                print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-                player2.checkIfDead()
-        elif int(player1.initiative) == int(player2.initiative):
-            player1.pdv = int(player1.pdv) - int(player2.niveau)
-            print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-            player2.pdv = int(player2.pdv) - int(player1.niveau)
-            print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-        return
-
-    def combat(player1, player2):
-        while int(player1.pdv) > 0 and int(player2.pdv) > 0:
-            player1.attaquer(player2)
-
-    def soigner(self):
-        soin = random.randint(1,self.niveau)
-        if self.pdv > 0:
-            self.pdv = self.pdv + soin
-            print(f"{self.pseudo} just healed himself. HP +{soin}")
-        elif self.pdv <= 0:
-            print(f"{self.pseudo} is dead and can't heal")
-        return
 
 class Mage(Personnage):
-    def __init__(self, pseudo:str, niveau:int, pdv:int, initiative:int, mana:int) -> None:
-        super().__init__(pseudo, niveau, pdv, initiative, mana)
+    def __init__(self, pseudo:str, level:int, hp:int, initiative:int, mana:int) -> None:
+        super().__init__(pseudo, level, hp, initiative, mana)
         self.pseudo = str(pseudo)
-        self.niveau = int(niveau)
-        self.pdv = int(niveau) * 5 + 10
-        self.initiative = int(niveau) * 6 + 4
-        self.mana = int(niveau) * 5
+        self.level = int(level)
+        self.hp = int(level) * 5 + 10
+        self.initiative = int(level) * 6 + 4
+        self.mana = int(level) * 5
 
     def __str__(self) -> str:
-        return (f"Type Mage => {self.pseudo} niveau {self.niveau} avec {self.pdv} points de vie. Initiative {self.initiative}")
+        return (f"Type Mage => {self.pseudo} level {self.level} with {self.hp} health. Initiative {self.initiative}. Mana {self.mana}")
 
-    def checkIfDead(player):
-        if player.pdv <= 0:
-            print(f'{player.pseudo} is dead !!')
-            return False
+    def damage(self):
+        if self.mana > 0:
+            damage = self.level + 3
+            self.mana = self.mana - 4
         else:
-            return True
-
-    def attaquer(player1, player2):
-        if int(player1.initiative) > int(player2.initiative) and player1.checkIfDead() and player2.checkIfDead():
-            player2.pdv = int(player2.pdv) - int(player1.niveau)
-            print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-            if player2.pdv <= 0:
-                return
-            else:
-                player1.pdv = int(player1.pdv) - int(player2.niveau)
-                print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-                player1.checkIfDead()
-        elif int(player1.initiative) < int(player2.initiative) and player2.checkIfDead() and player1.checkIfDead():
-            player1.pdv = int(player1.pdv) - int(player2.niveau)
-            print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-            if player1.pdv <= 0:
-                return
-            else:
-                player2.pdv = int(player2.pdv) - int(player1.niveau)
-                print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-                player2.checkIfDead()
-        elif int(player1.initiative) == int(player2.initiative):
-            player1.pdv = int(player1.pdv) - int(player2.niveau)
-            print(f"-{player2.niveau} hp to {player1.pseudo} (Current Hp : {player1.pdv})")
-            player2.pdv = int(player2.pdv) - int(player1.niveau)
-            print(f"-{player1.niveau} hp to {player2.pseudo} (Current Hp : {player2.pdv})")
-        return
-
-    def combat(player1, player2):
-        while int(player1.pdv) > 0 and int(player2.pdv) > 0:
-            player1.attaquer(player2)
-
-    def soigner(self):
-        soin = random.randint(1,self.niveau)
-        if self.pdv > 0:
-            self.pdv = self.pdv + soin
-            print(f"{self.pseudo} just healed himself. HP +{soin}")
-        elif self.pdv <= 0:
-            print(f"{self.pseudo} is dead and can't heal")
-        return
+            damage = self.level
+        return damage
 
 
 kevinou = Personnage("Kevan",10, 100, 11)
@@ -182,4 +108,6 @@ elea = Mage('Elea', 100, 50, 16, 100)
 anaita = Mage('Anaita', 110, 60, 14, 80)
 
 
-enzo.combat(anaita)
+enzo.fight(anaita)
+elea.fight(anaita)
+elea.fight(kevinou)
